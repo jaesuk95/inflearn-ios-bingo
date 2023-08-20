3eb2a55507d4b0d0aeb8cc3a0fae0a7df0f6a7f4
Jaesuk <jaesuk95@outlook.com>
2023-08-20 20:51:51 +0900

    빙고 게임, 핵심 내용 while 문

---
+++ RandomBingo/Rps.swift
@@ -0,0 +1,14 @@
+//
+//  Rps.swift
+//  RandomBingo
+//
+//  Created by Jaesuk Lee on 2023/08/20.
+//
+
+import Foundation
+
+enum Rps {
+    case rock
+    case paper
+    case scissors
+}
--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -9,3 +9,40 @@ import Foundation
 
 print("Hello, World!")
 
+
+
+//var a = Rps.paper
+
+// ! = make it not optional
+//let readLineText = readLine()!
+//print(readLineText)
+
+var computerhoice = Int.random(in: 1...100)
+print(computerhoice)
+
+// set as Integer
+var myChoice: Int = 0
+
+
+while true {
+    
+    // optional + click shows you the type of var
+    var userInput = readLine()
+    
+    // testing if input value is number
+    if let input = userInput  {     // optional 이여서 한번 if let input 를 사용
+        if let number = Int(input) {    // type 변형하기 위해서 number 변수에
+            myChoice = number
+        }
+    }
+    
+    if computerhoice > myChoice {
+        print("UP")
+    } else if computerhoice < myChoice {
+        print("down")
+    } else {
+        print("Bingo")
+        break
+    }
+    
+}
